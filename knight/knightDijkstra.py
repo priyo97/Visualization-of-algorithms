e3b0a5dc70d62cde425be1f9f50ba16e59ff0697
Priyabrata Saha <sahapriyabrata97@gmail.com>
2018-09-20 22:45:17 +0530

    knight Dijkstra version 1.1
    1. Improved code efficiency
    2. Only thing that has to be supplied from cmd is the grid size , source and target positions can now be provided using mouse interactively

--- knight/knightDijkstra.py
+++ knight/knightDijkstra.py
@@ -1,6 +1,9 @@
 import pygame
 import time
+import os
 from dijkstra import Dijkstra
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 def createChessboard(gameDisplay,n):
 
@@ -11,12 +14,25 @@ def createChessboard(gameDisplay,n):
 
 	for h in range(n):
 		for w in range(n):
+			
 			if (not h % 2 and not w % 2) or (h % 2 and w % 2):
 
-					x = w*xdiff
-					y = h*ydiff
+					x = w * xdiff
+					y = h * ydiff
 
 					pygame.draw.rect(gameDisplay,(0,0,0),(x,y,xdiff,ydiff))
+
+
+def loadEntity():
+
+	knight = pygame.image.load("knight.png")
+
+	yield 1,knight
+
+	queen = pygame.image.load("queen.png")
+
+	yield 2,queen
+
 
 
 def placeEntity(entity,x,y,gameDisplay):
@@ -25,31 +41,130 @@ def placeEntity(entity,x,y,gameDisplay):
 
 
 
+def main():
+
+	pygame.init()
+
+	# Taking input from user
+
+	n = int(input("Enter grid size:"))
+
+
+	
+	# Setting up the screen
+
+	res = (n*pixels,n*pixels)
+
+	gameDisplay = pygame.display.set_mode(res)
+
+
+	
+
+
+	createChessboard(gameDisplay,n)
+
+	pygame.display.update()
+
+	prev = (-1,-1)
+
+	pos = {}
+
+	entity = loadEntity()
+
+	running = True
+
+	while running:
+
+		for event in pygame.event.get():
+
+			if event.type == pygame.QUIT:
+
+				exit()
+
+			elif event.type == pygame.MOUSEBUTTONUP:
+
+				p = pygame.mouse.get_pos()
+
+				x = p[0]//50
+				y = p[1]//50
+
+
+				idx, e = next(entity)
+
+				placeEntity(e,x,y,gameDisplay)
+
+				pos[idx] = (x,y)
+
+				pygame.display.update()
+
+				if len(pos) == 2:
+
+					running = False
+					break
+
+
+
+			elif event.type == pygame.MOUSEMOTION:
+
+				p = pygame.mouse.get_pos()
+
+				w = p[0]//50
+				h = p[1]//50
+
+				if (w,h) not in pos.values():
+					
+					if prev != (w,h):
+
+						pygame.draw.rect(gameDisplay,(200,0,0),(w*50,h*50,50,50))
+
+						print("w h",(w,h),"prev",prev)
+
+						if prev not in pos.values():
+
+							if (not prev[1] % 2 and not prev[0] % 2) or (prev[1] % 2 and prev[0] % 2):
+
+								pygame.draw.rect(gameDisplay,(0,0,0),(prev[0]*50,prev[1]*50,50,50))
+							
+							else:
+								pygame.draw.rect(gameDisplay,(255,255,255),(prev[0]*50,prev[1]*50,50,50))
+
+						pygame.display.update()
+
+				else:
+
+					if prev not in pos.values():
+
+						if (not prev[1] % 2 and not prev[0] % 2) or (prev[1] % 2 and prev[0] % 2):
+
+							pygame.draw.rect(gameDisplay,(0,0,0),(prev[0]*50,prev[1]*50,50,50))
+						
+						else:
+							pygame.draw.rect(gameDisplay,(255,255,255),(prev[0]*50,prev[1]*50,50,50))
+
+						pygame.display.update()
+
+				prev = (w,h)
+	
+
+	time.sleep(1)
+
+	return n, pos , gameDisplay
+
+
 def markVisited(prevX,prevY,gameDisplay):
 
 	pygame.draw.rect(gameDisplay,(200,0,0),(prevX*pixels+5,prevY*pixels+5,pixels-10,pixels-10))
 
 
 
-def main():
+def rungame(n,pos,gameDisplay):
 
 	pygame.init()
 
+	Sy, Sx = pos[1]
+	Ty, Tx = pos[2]
 
-	# Taking input from user
-
-	n = int(input("Enter grid size:"))
-
-	Sx, Sy = [int(x) for x in input("Enter knight coords:").split()]
-	Tx, Ty = [int(x) for x in input("Enter queen coords:").split()]
-
-	
-
-	# Setting up the screen and images
-
-	res = (n*pixels,n*pixels)
-
-	gameDisplay = pygame.display.set_mode(res)
+	# Setting up images
 
 	queen  = pygame.image.load("queen.png")
 
@@ -118,6 +233,10 @@ def main():
 	pygame.quit()
 
 
+
+
 pixels = 50
 
-main()
+n, pos, gameDisplay = main()
+
+rungame(n,pos,gameDisplay)
